451108a38ff9959dcb59a52d2c7a12c340483e08
nic-jaskova <Nikki@Nicole-Jaskovas-MacBook-Pro.local>
2017-06-19 20:33:41 -0400

    june 19 - updated

---
+++ blog.js
@@ -0,0 +1,10 @@
+
+(function() {
+    var dialog = document.getElementById('window');
+    document.getElementById('show').onclick = function() {
+        dialog.show();
+    };
+    document.getElementById('exit').onclick = function() {
+        dialog.close();
+    };
+})();
