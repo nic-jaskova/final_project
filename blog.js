a44b98c39ece529118ccae86278d02c95435bc57
nic-jaskova <Nikki@Nicole-Jaskovas-MacBook-Pro.local>
2017-06-25 16:40:45 -0400

    june 25 popup added

--- blog.js
+++ blog.js
@@ -1,62 +1,17 @@
+$(document).ready(function() {
+  $(".open").click (function () {
+  $('.pop-outer').fadeIn('slow');
+  });
+$(".close").click(function() {
+  $('.pop-outer').fadeOut('slow');
+  });
+});
 
-$(function () {
-
-  function handleSubmit(evt) {
-    evt.preventDefault();
-
-    var $form = $(this);
-    var $errorMessage = $('#errorMessage');
-
-    $form.hide();
-
-    console.log('Sending....')
-
-
-    // collect the data
-    var payload = {
-      hello: 'world',
-    };
-    var formData = $form.serializeArray();
-
-    /*for (var i = 0; i < formData.length; i++) {
-      var fieldObj = formData[i];
-      var fieldName = fieldObj.name;
-      var fieldValue = fieldObj.value;
-      //
-    }*/
-
-    // iterate over serialized data
-    formData.forEach(function (fieldObj) {
-      var fieldName = fieldObj.name;
-      var fieldValue = fieldObj.value;
-
-      payload[fieldName] = fieldValue;
-    });
-
-    // console.log('payload after', payload)
-
-    // Send to the server
-    var endpoint = 'https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6';
-    /*$.ajax({
-      method: 'post',
-      url: endpoint,
-      data: payload
-    })*/
-
-    $.post(endpoint, payload)
-      .done(function () {
-        console.log('successs!!!!', data)
-      })
-      .fail(function (err) {
-        console.error('error!!!', err)
-        var errorMessage = err.responseJSON.message;
-        console.log('Finished!!')
-
-        $errorMessage.text(errorMessage);
-        // alert(errorMessage)
-        // $form.show()
-      });
-  }
-
-  $('#submitForm').on('Submit', handleSubmit);
+$(document).ready(function() {
+  $(".open2").click (function () {
+    $('.pop-outer2').fadeIn('slow');
+  });
+$(".close2").click(function() {
+  $('.pop-outer2').fadeOut('slow');
+  });
 });
