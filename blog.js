f4f368194988513f997da8c0504dc6fcb94417d3
nic-jaskova <Nikki@Nicole-Jaskovas-MacBook-Pro.local>
2017-06-25 20:28:11 -0400

    june 25 updated for iPhone

--- blog.js
+++ blog.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
   $(".open").click (function () {
-  $('.pop-outer').fadeIn('slow');
+    $('.pop-outer').fadeIn('slow');
+  });
   });
 $(".close").click(function() {
   $('.pop-outer').fadeOut('slow');
   });
-});
-
 $(document).ready(function() {
   $(".open2").click (function () {
     $('.pop-outer2').fadeIn('slow');
   });
+  });
 $(".close2").click(function() {
   $('.pop-outer2').fadeOut('slow');
   });
-});
+$(document).ready(function() {
+  $(".open3").click (function () {
+    $('.pop-outer3').fadeIn('slow');
+  });
+  });
+$(".close3").click(function() {
+  $('.pop-outer3').fadeOut('slow');
+  });
+$(document).ready(function() {
+    $(".open4").click (function () {
+      $('.pop-outer4').fadeIn('slow');
+    });
+    });
+  $(".close4").click(function() {
+    $('.pop-outer4').fadeOut('slow');
+    });
